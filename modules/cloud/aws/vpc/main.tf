0c897e400ab127ea75d7a00f3752a5a34a936549
Ali Abbas Jaffri <ali.abbasjaffri@live.com>
2023-05-25 21:20:35 +0200

    Added initial draft of modules
    Signed-off-by: Ali Abbas Jaffri <ali.abbasjaffri@live.com>

---
+++ modules/cloud/aws/vpc/main.tf
@@ -0,0 +1,32 @@
+resource "aws_eip" "nat" {
+  count = 3
+
+  vpc = true
+}
+
+module "vpc_subnet_module" {
+  source = "terraform-aws-modules/vpc/aws"
+  # version = var.vpc_subnet_module.version
+  version = "4.0.2"
+
+  name = "my-vpc"
+  cidr = "10.0.0.0/16"
+
+  azs             = ["eu-west-1a", "eu-west-1b", "eu-west-1c"]
+  private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+
+  enable_nat_gateway  = true
+  single_nat_gateway  = false
+  reuse_nat_ips       = true             # <= Skip creation of EIPs for the NAT Gateways
+  external_nat_ip_ids = aws_eip.nat.*.id # <= IPs specified here as input to the module
+  enable_vpn_gateway  = false
+
+  enable_dns_hostnames = true
+  enable_dns_support   = true
+
+  tags = {
+    Terraform   = "true"
+    Environment = "dev"
+  }
+}
