2c0cf03b4cfd1fb1c8539211f1dd6de1cd0079fc
Ali Abbas Jaffri <ali.abbasjaffri@live.com>
2023-06-05 21:38:43 +0200

    Updated dynamic AZs selection
    Signed-off-by: Ali Abbas Jaffri <ali.abbasjaffri@live.com>

--- modules/cloud/aws/vpc/main.tf
+++ modules/cloud/aws/vpc/main.tf
@@ -1,8 +1,9 @@
 resource "aws_eip" "nat" {
   count = 3
-
-  vpc = true
+  vpc   = true
 }
+
+data "aws_availability_zones" "available" {}
 
 module "vpc_subnet_module" {
   source = "terraform-aws-modules/vpc/aws"
@@ -12,7 +13,7 @@ module "vpc_subnet_module" {
   name = "my-vpc"
   cidr = "10.0.0.0/16"
 
-  azs             = ["eu-west-1a", "eu-west-1b", "eu-west-1c"]
+  azs             = slice(data.aws_availability_zones.available.names, 0, 3)
   private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
   public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
 
