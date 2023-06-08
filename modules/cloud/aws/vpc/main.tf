1d50143dad84e50ffaa603ca5a29d87096cac529
Ali Abbas Jaffri <ali.abbasjaffri@live.com>
2023-06-08 15:06:20 +0200

    Generic AZS based on provider config
    Signed-off-by: Ali Abbas Jaffri <ali.abbasjaffri@live.com>

--- modules/cloud/aws/vpc/main.tf
+++ modules/cloud/aws/vpc/main.tf
@@ -5,6 +5,12 @@ resource "aws_eip" "nat" {
 
 data "aws_availability_zones" "available" {}
 
+locals {
+  vpc_cidr = "10.0.0.0/16"
+  azs      = slice(data.aws_availability_zones.available.names, 0, 3)
+}
+
+
 module "vpc_subnet_module" {
   source = "terraform-aws-modules/vpc/aws"
   # version = var.vpc_subnet_module.version
@@ -13,9 +19,9 @@ module "vpc_subnet_module" {
   name = "my-vpc"
   cidr = "10.0.0.0/16"
 
-  azs             = slice(data.aws_availability_zones.available.names, 0, 3)
-  private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
-  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+  azs             = local.azs
+  private_subnets = [for k, v in local.azs : cidrsubnet(local.vpc_cidr, 8, k)]
+  public_subnets  = [for k, v in local.azs : cidrsubnet(local.vpc_cidr, 8, k + 4)]
 
   enable_nat_gateway  = true
   single_nat_gateway  = false
