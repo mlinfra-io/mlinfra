e304f971e6ced9087569926fcad6abd260ee81d4
Ali Abbas Jaffri <ali.abbasjaffri@live.com>
2023-06-08 20:09:22 +0200

    Added configuration for flow logs for VPC
    Signed-off-by: Ali Abbas Jaffri <ali.abbasjaffri@live.com>

--- modules/cloud/aws/vpc/main.tf
+++ modules/cloud/aws/vpc/main.tf
@@ -1,6 +1,5 @@
 resource "aws_eip" "nat" {
   count = 3
-  vpc   = true
 }
 
 data "aws_availability_zones" "available" {}
@@ -8,6 +7,38 @@ data "aws_availability_zones" "available" {}
 locals {
   vpc_cidr = "10.0.0.0/16"
   azs      = slice(data.aws_availability_zones.available.names, 0, 3)
+  tags = {
+    Terraform   = "true"
+    Environment = "dev"
+  }
+}
+
+resource "aws_s3_bucket" "vpc_logs_bucket" {
+  bucket = "vpc-flow-logs-bucket"
+  tags   = local.tags
+}
+
+resource "aws_s3_bucket_acl" "vpc_logs_bucket_acl" {
+  bucket = aws_s3_bucket.vpc_logs_bucket.id
+  acl    = "private"
+}
+
+resource "aws_s3_bucket_lifecycle_configuration" "flow_logs_lifecycle" {
+  bucket = aws_s3_bucket.vpc_logs_bucket.id
+
+  rule {
+    id     = "MoveLogsToGlacier"
+    status = "Enabled"
+
+    transition {
+      days          = 10
+      storage_class = "GLACIER"
+    }
+
+    expiration {
+      days = 60
+    }
+  }
 }
 
 
@@ -23,8 +54,10 @@ module "vpc_subnet_module" {
   private_subnets = [for k, v in local.azs : cidrsubnet(local.vpc_cidr, 8, k)]
   public_subnets  = [for k, v in local.azs : cidrsubnet(local.vpc_cidr, 8, k + 4)]
 
-  enable_nat_gateway  = true
-  single_nat_gateway  = false
+  enable_nat_gateway     = true
+  single_nat_gateway     = false
+  one_nat_gateway_per_az = false
+
   reuse_nat_ips       = true             # <= Skip creation of EIPs for the NAT Gateways
   external_nat_ip_ids = aws_eip.nat.*.id # <= IPs specified here as input to the module
   enable_vpn_gateway  = false
@@ -32,8 +65,12 @@ module "vpc_subnet_module" {
   enable_dns_hostnames = true
   enable_dns_support   = true
 
-  tags = {
-    Terraform   = "true"
-    Environment = "dev"
-  }
+  enable_flow_log                   = true
+  flow_log_destination_type         = "s3"
+  flow_log_destination_arn          = aws_s3_bucket.vpc_logs_bucket.arn
+  flow_log_max_aggregation_interval = 600
+
+  manage_default_network_acl = true
+
+  tags = local.tags
 }
