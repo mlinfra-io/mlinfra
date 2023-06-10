b3570c6e189ab5aefcc1eaa406a824109e00f140
Ali Abbas Jaffri <ali.abbasjaffri@live.com>
2023-06-10 17:20:57 +0200

    Added filter for flowlogs bucket path
    Signed-off-by: Ali Abbas Jaffri <ali.abbasjaffri@live.com>

--- modules/cloud/aws/vpc/vpc_flow_logs.tf
+++ modules/cloud/aws/vpc/vpc_flow_logs.tf
@@ -7,6 +7,10 @@ resource "aws_s3_bucket_lifecycle_configuration" "flow_logs_lifecycle" {
   bucket = aws_s3_bucket.vpc_logs_bucket.id
 
   rule {
+    filter {
+      prefix = "AWSLogs/"
+    }
+
     id     = "MoveLogsToGlacier"
     status = "Enabled"
 
