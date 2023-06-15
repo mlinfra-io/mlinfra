2adc110332aadd4775d3d046770c2f0d5b7495ed
Ali Abbas Jaffri <ali.abbasjaffri@live.com>
2023-06-15 21:48:17 +0200

    Added terraform code for deploying wandb tracker
    Signed-off-by: Ali Abbas Jaffri <ali.abbasjaffri@live.com>

---
+++ modules/applications/experiment_tracking/wandb/tf_module/main.tf
@@ -0,0 +1,51 @@
+# TODO: Replace with module from Anton Babenko
+resource "aws_s3_bucket" "wandb_artifacts_bucket" {
+  count  = var.remote_tracking ? 1 : 0
+  bucket = "ultimate-wandb-artifacts-storage-bucket"
+  tags   = var.tags
+}
+
+# create rds instance
+module "wandb_rds_backend" {
+  source = "../../../../cloud/aws/rds"
+
+  create_rds = var.remote_tracking
+
+  # TODO: pass these variables generically
+  # via the json file
+  vpc_id               = var.vpc_id
+  db_subnet_group_name = var.db_subnet_group_name
+  vpc_cidr_block       = var.vpc_cidr_block
+
+  rds_identifier = "wandb-backend"
+  db_name        = "wandbbackend"
+  db_username    = "wandb_backend_user"
+  tags           = var.tags
+}
+
+module "wandb" {
+  source                  = "../../../../cloud/aws/ec2"
+  vpc_id                  = var.vpc_id
+  default_vpc_sg          = var.default_vpc_sg
+  vpc_cidr_block          = var.vpc_cidr_block
+  ec2_subnet_id           = var.subnet_id
+  ec2_instance_name       = "wandb-server"
+  ec2_spot_instance       = var.ec2_spot_instance
+  ec2_application_port    = var.ec2_application_port
+  enable_rds_ingress_rule = var.remote_tracking
+
+  ec2_user_data = var.remote_tracking ? templatefile("${path.module}/remote-cloud-init.tpl", {
+    wandb_version        = var.wandb_version
+    ec2_application_port = var.ec2_application_port
+    db_instance_username = module.wandb_rds_backend.db_instance_username
+    db_instance_password = module.wandb_rds_backend.db_instance_password
+    db_instance_endpoint = module.wandb_rds_backend.db_instance_endpoint
+    db_instance_name     = module.wandb_rds_backend.db_instance_name
+    bucket_id            = aws_s3_bucket.wandb_artifacts_bucket[0].id
+    }) : templatefile("${path.module}/simple-cloud-init.tpl", {
+    wandb_version        = var.wandb_version
+    ec2_application_port = var.ec2_application_port
+  })
+
+  depends_on = [resource.aws_s3_bucket.wandb_artifacts_bucket, module.wandb_rds_backend]
+}
