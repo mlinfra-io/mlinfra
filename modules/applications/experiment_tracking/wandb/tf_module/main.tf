72b4d2e5019c7b624f967ef4a8974e50e3b3e628
Ali Abbas Jaffri <ali.abbasjaffri@live.com>
2023-06-17 16:35:44 +0200

    Added experiement tracker wandb
    Signed-off-by: Ali Abbas Jaffri <ali.abbasjaffri@live.com>

--- modules/applications/experiment_tracking/wandb/tf_module/main.tf
+++ modules/applications/experiment_tracking/wandb/tf_module/main.tf
@@ -5,23 +5,83 @@ resource "aws_s3_bucket" "wandb_artifacts_bucket" {
   tags   = var.tags
 }
 
+resource "aws_iam_policy" "s3_access_iam_policy" {
+  name        = "S3AccessPolicy"
+  description = "Allows access to S3 bucket"
+
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Action": [
+        "s3:GetObject",
+        "s3:ListBucket",
+        "s3:PutObject"
+      ],
+      "Resource": [
+        "${aws_s3_bucket.wandb_artifacts_bucket[0].arn}",
+        "${aws_s3_bucket.wandb_artifacts_bucket[0].arn}/*"
+      ]
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_role" "ec2_iam_role" {
+  name = "EC2RoleWithS3Access"
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "ec2.amazonaws.com"
+      },
+      "Effect": "Allow",
+      "Sid": ""
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_role_policy_attachment" "s3_access_attachment" {
+  role       = aws_iam_role.ec2_iam_role.name
+  policy_arn = aws_iam_policy.s3_access_iam_policy.arn
+}
+
+resource "aws_iam_instance_profile" "wandb_instance_profile" {
+  name = "wandb_instance_profile"
+  role = aws_iam_role.ec2_iam_role.name
+}
+
 # create rds instance
 module "wandb_rds_backend" {
   source = "../../../../cloud/aws/rds"
 
-  create_rds = var.remote_tracking
-
-  # TODO: pass these variables generically
-  # via the json file
   vpc_id               = var.vpc_id
   db_subnet_group_name = var.db_subnet_group_name
   vpc_cidr_block       = var.vpc_cidr_block
+
+  create_rds               = var.remote_tracking
+  rds_engine               = "mysql"
+  rds_engine_version       = "8.0"
+  rds_family               = "mysql8.0" # DB parameter group
+  rds_major_engine_version = "8.0"      # DB option group
+  rds_port                 = 3306
 
   rds_identifier = "wandb-backend"
   db_name        = "wandbbackend"
   db_username    = "wandb_backend_user"
   tags           = var.tags
 }
+
+data "aws_region" "current" {}
 
 module "wandb" {
   source                  = "../../../../cloud/aws/ec2"
@@ -32,11 +92,13 @@ module "wandb" {
   ec2_instance_name       = "wandb-server"
   ec2_spot_instance       = var.ec2_spot_instance
   ec2_application_port    = var.ec2_application_port
+  iam_instance_profile    = var.remote_tracking ? aws_iam_instance_profile.wandb_instance_profile.name : null
   enable_rds_ingress_rule = var.remote_tracking
 
   ec2_user_data = var.remote_tracking ? templatefile("${path.module}/remote-cloud-init.tpl", {
     wandb_version        = var.wandb_version
     ec2_application_port = var.ec2_application_port
+    aws_region           = data.aws_region.current.name
     db_instance_username = module.wandb_rds_backend.db_instance_username
     db_instance_password = module.wandb_rds_backend.db_instance_password
     db_instance_endpoint = module.wandb_rds_backend.db_instance_endpoint
--- modules/applications/experiment_tracking/wandb/tf_module/remote-cloud-init.tpl
+++ modules/applications/experiment_tracking/wandb/tf_module/remote-cloud-init.tpl
@@ -7,8 +7,9 @@ sudo groupadd docker
 sudo usermod -aG docker $USER
 python3 -m pip install --ignore-installed --upgrade pip
 python3 -m pip install --ignore-installed wandb==${wandb_version}
-nohup wandb server start --port ${ec2_application_port} --daemon \
+nohup wandb server start --port ${ec2_application_port} \
     -e AWS_REGION=${aws_region} \
     -e MYSQL=mysql://${db_instance_username}:${db_instance_password}@${db_instance_endpoint}/${db_instance_name} \
-    -e BUCKET=s3://${bucket_id} &
-#    -e BUCKET_QUEUE= &
+    # -e BUCKET=s3://${bucket_id} \
+    # -e BUCKET_QUEUE= \
+    --daemon &
