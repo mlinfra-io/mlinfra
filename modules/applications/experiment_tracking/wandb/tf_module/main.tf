026c518bb6d96b7d06c9442d07b8d4ac86fe75fd
Ali Abbas Jaffri <ali.abbasjaffri@live.com>
2023-06-19 10:05:38 +0200

    Configured ec2 instance type as modifyable for wandb
    Signed-off-by: Ali Abbas Jaffri <ali.abbasjaffri@live.com>

--- modules/applications/experiment_tracking/wandb/tf_module/main.tf
+++ modules/applications/experiment_tracking/wandb/tf_module/main.tf
@@ -143,6 +143,7 @@ module "wandb" {
   ec2_spot_instance       = var.ec2_spot_instance
   ec2_application_port    = var.ec2_application_port
   iam_instance_profile    = var.remote_tracking ? aws_iam_instance_profile.wandb_instance_profile[0].name : null
+  ec2_instance_type       = var.ec2_instance_type
   enable_rds_ingress_rule = var.remote_tracking
 
   ec2_user_data = var.remote_tracking ? templatefile("${path.module}/remote-cloud-init.tpl", {
