1d3084037ca9ec2987c9fb7224a875c2727813f3
Ali Abbas Jaffri <ali.abbasjaffri@live.com>
2023-07-28 11:26:19 +0200

    Added cost estimator
    Signed-off-by: Ali Abbas Jaffri <ali.abbasjaffri@live.com>

--- tasks.py
+++ tasks.py
@@ -9,6 +9,36 @@ from ultimate_mlops_cli.terraform.terraform_state_helper import TerraformStateHe
     pre=[],
     help={
         "stack_config_path": "Path of the config file",
+    },
+)
+def estimate_cost(
+    ctx,
+    stack_config_path: str,
+    args: str = "",
+):
+    # clean the tf directory before init
+    clean_tf_directory()
+
+    file_processor = StackfileProcessor(stack_config_path=stack_config_path)
+
+    state_helper = TerraformStateHelper(
+        state=file_processor.get_state_file_name(), region=file_processor.get_region()
+    )
+    state_helper.manage_aws_state_storage()
+
+    file_processor.generate()
+
+    ctx.run(f"cd {TF_PATH} && terraform init")
+    ctx.run(
+        f"cd {TF_PATH} && terraform plan -lock=false -out tfplan.binary && terraform show -json tfplan.binary > plan.json"
+    )
+    ctx.run(f"infracost diff --show-skipped --no-cache --path {TF_PATH}/plan.json")
+
+
+@task(
+    pre=[],
+    help={
+        "stack_config_path": "Path of the config file",
         "action": "Allowed actions are plan, destroy, apply, force-unlock",
     },
 )
