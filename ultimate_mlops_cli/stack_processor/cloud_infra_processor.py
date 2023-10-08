f4efccbcb369efb10f3cd88e4489d431825ceb0a
Ali Abbas Jaffri <ali.abbasjaffri@live.com>
2023-10-08 23:27:55 +0200

    Added stub definition for cloud infra preprocessor
    Signed-off-by: Ali Abbas Jaffri <ali.abbasjaffri@live.com>

--- ultimate_mlops_cli/stack_processor/cloud_infra_processor.py
+++ ultimate_mlops_cli/stack_processor/cloud_infra_processor.py
@@ -6,5 +6,5 @@ class CloudInfraProcessor(BaseProcessor):
     def __init__(self, config):
         super(CloudInfraProcessor, self).__init__(config)
 
-    def process(self):
+    def generate(self):
         logger.info("Processing Cloud Infrastructure")
