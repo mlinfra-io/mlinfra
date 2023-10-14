a1e67992e1ceab61a96594ff3b20b73e9205ffb6
Ali Abbas Jaffri <ali.abbasjaffri@live.com>
2023-10-15 01:49:05 +0200

    Updated provider logic
    Signed-off-by: Ali Abbas Jaffri <ali.abbasjaffri@live.com>

--- ultimate_mlops_cli/stack_processor/provider_processor/aws_provider.py
+++ ultimate_mlops_cli/stack_processor/provider_processor/aws_provider.py
@@ -1,17 +1,14 @@
+import json
 import yaml
 from ultimate_mlops_cli.stack_processor.provider_processor.provider import (
     AbstractProvider,
 )
+from ultimate_mlops_cli.utils.constants import TF_PATH
 
 
 class AWSProvider(AbstractProvider):
     def __init__(self, config: yaml):
-        self.config = config
-        self.account_id = config.get("account_id", None)
-        self.region = config.get("region", None)
-        self.access_key = config.get("access_key", None)
-        self.secret_key = config.get("secret_key", None)
-        self.role_arn = config.get("role_arn", None)
+        super().__init__(config=config)
 
     def get_provider_details(self) -> (str, str):
         super().get_provider_details()
@@ -22,3 +19,29 @@ class AWSProvider(AbstractProvider):
 
     def get_role_arn(self) -> str:
         return self.role_arn
+
+    def configure_provider(self):
+        with open(
+            f"modules/cloud/{self.provider.value}/provider.tf.json", "r"
+        ) as data_json:
+            with open(
+                f"./{TF_PATH}/provider.tf.json", "w", encoding="utf-8"
+            ) as tf_json:
+                data = json.load(data_json)
+                data["provider"]["aws"]["region"] = self.region
+                data["provider"]["aws"]["allowed_account_ids"] = [self.account_id]
+                data["provider"]["aws"]["default_tags"]["tags"]["region"] = self.region
+                data["provider"]["aws"]["default_tags"]["tags"][
+                    "stack"
+                ] = self.stack_config_path
+
+                # TODO: Add assume role block if applicable
+
+                # add random provider
+                with open(
+                    "modules/terraform_providers/random/provider.tf.json", "r"
+                ) as random_provider:
+                    random_provider_json = json.load(random_provider)
+                data["provider"].update(random_provider_json["provider"])
+
+                json.dump(data, tf_json, ensure_ascii=False, indent=2)
--- ultimate_mlops_cli/stack_processor/provider_processor/provider.py
+++ ultimate_mlops_cli/stack_processor/provider_processor/provider.py
@@ -2,6 +2,27 @@ from abc import ABC, abstractmethod
 
 
 class AbstractProvider(ABC):
+    """
+    Abstract class for providers
+    """
+
     @abstractmethod
-    def get_provider_details(self):
+    def __init__(self, config):
+        self.config = config
+        self.account_id = config.get("account_id", None)
+        self.region = config.get("region", None)
+        self.access_key = config.get("access_key", None)
+        self.secret_key = config.get("secret_key", None)
+        self.role_arn = config.get("role_arn", None)
+
+    @abstractmethod
+    def get_provider_details(self) -> (str, str):
+        pass
+
+    @abstractmethod
+    def get_access_credentials(self) -> (str, str):
+        pass
+
+    @abstractmethod
+    def configure_provider(self):
         pass
