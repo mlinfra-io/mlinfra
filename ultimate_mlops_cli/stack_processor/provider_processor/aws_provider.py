b863397061eb6e374e1cff1dd811acbac23f26d9
Ali Abbas Jaffri <ali.abbasjaffri@live.com>
2023-10-28 17:48:49 +0200

    Refactored provider logic
    Signed-off-by: Ali Abbas Jaffri <ali.abbasjaffri@live.com>

--- ultimate_mlops_cli/stack_processor/provider_processor/aws_provider.py
+++ ultimate_mlops_cli/stack_processor/provider_processor/aws_provider.py
@@ -18,6 +18,10 @@ class AWSProvider(AbstractProvider):
         self.secret_key = config.get("secret_key", None)
         self.role_arn = config.get("role_arn", None)
 
+    # TODO: refactor statefile name
+    def get_statefile_name(self) -> str:
+        return f"tfstate-{self.stack_name}-{self.region}"
+
     def configure_provider(self):
         with open("modules/cloud/aws/provider.tf.json", "r") as data_json:
             with open(
@@ -27,9 +31,9 @@ class AWSProvider(AbstractProvider):
                 data["provider"]["aws"]["region"] = self.region
                 data["provider"]["aws"]["allowed_account_ids"] = [self.account_id]
                 data["provider"]["aws"]["default_tags"]["tags"]["region"] = self.region
-                data["provider"]["aws"]["default_tags"]["tags"][
-                    "stack"
-                ] = self.stack_config_path
+                # data["provider"]["aws"]["default_tags"]["tags"][
+                #     "stack"
+                # ] = self.stack_config_path
 
                 # TODO: Add assume role block if applicable
 
@@ -42,17 +46,21 @@ class AWSProvider(AbstractProvider):
 
                 json.dump(data, tf_json, ensure_ascii=False, indent=2)
 
-        with open("modules/cloud/aws/data.tf.json", "r") as data_json:
-            with open(f"./{TF_PATH}/data.tf.json", "w", encoding="utf-8") as tf_json:
-                json_data = json.load(data_json)
+        # TODO: check when to add this...
 
-                json_data["data"]["terraform_remote_state"] = {
-                    "backend": "s3",
-                    "config": {
-                        "bucket": self.stack_name,
-                        "key": "ultimate-mlops-stack",
-                        "dynamodb_table": self.stack_name,
-                        "region": self.region,
-                    },
-                }
-                json.dump(json_data, tf_json, ensure_ascii=False, indent=2)
+        # with open("modules/cloud/aws/data.tf.json", "r") as data_json:
+        #     with open(f"./{TF_PATH}/data.tf.json", "w", encoding="utf-8") as tf_json:
+        #         json_data = json.load(data_json)
+
+        #         json_data["data"]["terraform_remote_state"] = {
+        #             "parent": {
+        #                 "backend": "s3",
+        #                 "config": {
+        #                     "bucket": self.get_statefile_name(),
+        #                     "key": "ultimate-mlops-stack",
+        #                     "dynamodb_table": self.get_statefile_name(),
+        #                     "region": self.region,
+        #                 },
+        #             }
+        #         }
+        #         json.dump(json_data, tf_json, ensure_ascii=False, indent=2)
