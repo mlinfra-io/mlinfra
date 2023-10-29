ac942ff511b24af186632ed31205d043de8719f7
Ali Abbas Jaffri <ali.abbasjaffri@live.com>
2023-10-29 10:17:48 +0100

    Added eks cluster
    Signed-off-by: Ali Abbas Jaffri <ali.abbasjaffri@live.com>

---
+++ modules/cloud/aws/eks/main.tf
@@ -0,0 +1,254 @@
+data "aws_caller_identity" "current" {}
+
+locals {
+  cluster_tags = merge({
+    Name   = var.aws_eks_cluster.cluster_name,
+    Module = path.module
+    }, var.aws_eks_cluster.tags
+  )
+}
+
+resource "aws_kms_key" "eks" {
+  description             = var.aws_kms_key.description
+  deletion_window_in_days = var.aws_kms_key.deletion_window_in_days
+  enable_key_rotation     = var.aws_kms_key.enable_key_rotation
+  tags                    = var.aws_kms_key.tags
+}
+
+resource "aws_security_group" "additional_security_group" {
+  name_prefix = var.aws_security_group.name_prefix
+  vpc_id      = var.aws_security_group.vpc_id
+
+  dynamic "ingress" {
+    for_each = var.aws_security_group.ingresses
+    content {
+      from_port   = ingress.value.from_port
+      to_port     = ingress.value.to_port
+      protocol    = ingress.value.protocol
+      cidr_blocks = ingress.value.cidr_blocks
+    }
+  }
+  tags = var.aws_security_group.tags
+}
+
+module "eks" {
+  source  = "terraform-aws-modules/eks/aws"
+  version = "~> 19.0"
+
+  cluster_name                    = var.aws_eks_cluster.cluster_name
+  cluster_version                 = var.aws_eks_cluster.cluster_version
+  cluster_endpoint_private_access = var.aws_eks_cluster.cluster_endpoint_private_access
+  cluster_endpoint_public_access  = var.aws_eks_cluster.cluster_endpoint_public_access
+
+  vpc_id = var.aws_eks_cluster.vpc_id
+  # set to private subnets if cluster is in private subnets
+  subnet_ids        = var.aws_eks_cluster.subnets
+  cluster_ip_family = var.aws_eks_cluster.cluster_ip_family
+  # set to private subnets if cluster is in private subnets
+  control_plane_subnet_ids = var.aws_eks_cluster.control_plane_subnet_ids
+
+  create_kms_key = false
+  cluster_encryption_config = [{
+    provider_key_arn = aws_kms_key.eks.arn
+    resources        = ["secrets"]
+  }]
+
+  cluster_addons = {
+    vpc-cni = {
+      # Reference docs https://docs.aws.amazon.com/eks/latest/userguide/cni-increase-ip-addresses.html
+      before_compute              = true
+      most_recent                 = true
+      resolve_conflicts_on_create = "OVERWRITE"
+      resolve_conflicts_on_update = "PRESERVE"
+      service_account_role_arn    = module.vpc_cni_irsa.iam_role_arn
+      configuration_values = {
+        env = {
+          ENABLE_PREFIX_DELEGATION = "true"
+          WARM_PREFIX_TARGET       = "1"
+        }
+      }
+    }
+    coredns = {
+      most_recent                 = true
+      resolve_conflicts_on_create = "OVERWRITE"
+      resolve_conflicts_on_update = "PRESERVE"
+      configuration_values = jsonencode({
+        affinity = {
+          nodeAffinity = {
+            requiredDuringSchedulingIgnoredDuringExecution = {
+              nodeSelectorTerms = [{
+                matchExpressions = [{
+                  key      = "nodegroup_type"
+                  operator = "In"
+                  values   = "operations"
+                }]
+              }]
+            }
+          }
+        }
+        tolerations = [{
+          key      = "nodegroup_type"
+          operator = "Equal"
+          value    = "operations"
+          effect   = "NoSchedule"
+        }]
+      })
+    }
+    kube-proxy = {
+      most_recent                 = true
+      resolve_conflicts_on_create = "OVERWRITE"
+      resolve_conflicts_on_update = "PRESERVE"
+    }
+    aws-ebs-csi-driver = {
+      most_recent                 = true
+      resolve_conflicts_on_create = "OVERWRITE"
+      resolve_conflicts_on_update = "PRESERVE"
+      service_account_role_arn    = module.ebs_csi_driver_irsa.iam_role_arn
+      configuration_values = jsonencode({
+        controller = {
+          affinity = {
+            nodeAffinity = {
+              requiredDuringSchedulingIgnoredDuringExecution = {
+                nodeSelectorTerms = [{
+                  matchExpressions = [{
+                    key      = "nodegroup_type"
+                    operator = "In"
+                    values   = "operations"
+                  }]
+                }]
+              }
+            }
+          }
+          tolerations = [{
+            key      = "nodegroup_type"
+            operator = "Equal"
+            value    = "operations"
+            effect   = "NoSchedule"
+          }]
+        }
+        node = {
+          affinity = {
+            nodeAffinity = {
+              requiredDuringSchedulingIgnoredDuringExecution = {
+                nodeSelectorTerms = [{
+                  matchExpressions = [{
+                    key      = "nodegroup_type"
+                    operator = "In"
+                    values   = "operations"
+                  }]
+                }]
+              }
+            }
+          }
+          tolerations = [{
+            key      = "nodegroup_type"
+            operator = "Equal"
+            value    = "operations"
+            effect   = "NoSchedule"
+          }]
+        }
+      })
+    }
+  }
+
+  # Extend cluster security group rules
+  cluster_security_group_additional_rules = var.aws_eks_cluster.cluster_security_group_additional_rules
+
+  # Extend node-to-node security group rules
+  node_security_group_additional_rules = var.aws_eks_cluster.node_security_group_additional_rules
+
+  eks_managed_node_group_defaults = {}
+
+  eks_managed_node_groups = var.aws_eks_cluster.eks_managed_node_groups
+
+  # update tags
+  tags = var.aws_eks_cluster.tags
+}
+
+module "vpc_cni_irsa" {
+  source  = "terraform-aws-modules/iam/aws//modules/iam-role-for-service-accounts-eks"
+  version = "~> 5.30"
+
+  role_name_prefix      = var.vpc_cni_irsa.role_name_prefix
+  attach_vpc_cni_policy = var.vpc_cni_irsa.attach_vpc_cni_policy
+  vpc_cni_enable_ipv4   = var.vpc_cni_irsa.vpc_cni_enable_ipv4
+
+  oidc_providers = {
+    main = {
+      provider_arn               = module.eks.oidc_provider_arn
+      namespace_service_accounts = ["kube-system:aws-node"]
+    }
+  }
+
+  # update tags
+  tags = var.aws_eks_cluster.tags
+}
+
+module "ebs_csi_driver_irsa" {
+  source  = "terraform-aws-modules/iam/aws//modules/iam-role-for-service-accounts-eks"
+  version = "~> 5.30"
+
+  role_name_prefix      = "EBS-CSI-IRSA"
+  attach_ebs_csi_policy = true
+
+  oidc_providers = {
+    main = {
+      provider_arn               = module.eks.oidc_provider_arn
+      namespace_service_accounts = ["kube-system:ebs-csi-controller-sa"]
+    }
+  }
+
+  # update tags
+  tags = var.aws_eks_cluster.tags
+}
+
+# source:
+# https://github.com/terraform-aws-modules/terraform-aws-eks/blob/51cc6bec880ac8dc361b60a4b05d5f2bcd98eb6a/examples/eks_managed_node_group/main.tf#L462-L509
+################################################################################
+# Tags for the ASG to support cluster-autoscaler scale up from 0
+################################################################################
+
+locals {
+
+  # We need to lookup K8s taint effect from the AWS API value
+  taint_effects = {
+    NO_SCHEDULE        = "NoSchedule"
+    NO_EXECUTE         = "NoExecute"
+    PREFER_NO_SCHEDULE = "PreferNoSchedule"
+  }
+
+  cluster_autoscaler_label_tags = merge([
+    for name, group in module.eks.eks_managed_node_groups : {
+      for label_name, label_value in coalesce(group.node_group_labels, {}) : "${name}|label|${label_name}" => {
+        autoscaling_group = group.node_group_autoscaling_group_names[0],
+        key               = "k8s.io/cluster-autoscaler/node-template/label/${label_name}",
+        value             = label_value,
+      }
+    }
+  ]...)
+
+  cluster_autoscaler_taint_tags = merge([
+    for name, group in module.eks.eks_managed_node_groups : {
+      for taint in coalesce(group.node_group_taints, []) : "${name}|taint|${taint.key}" => {
+        autoscaling_group = group.node_group_autoscaling_group_names[0],
+        key               = "k8s.io/cluster-autoscaler/node-template/taint/${taint.key}"
+        value             = "${taint.value}:${local.taint_effects[taint.effect]}"
+      }
+    }
+  ]...)
+
+  cluster_autoscaler_asg_tags = merge(local.cluster_autoscaler_label_tags, local.cluster_autoscaler_taint_tags)
+}
+
+resource "aws_autoscaling_group_tag" "cluster_autoscaler_label_tags" {
+  for_each = local.cluster_autoscaler_asg_tags
+
+  autoscaling_group_name = each.value.autoscaling_group
+
+  tag {
+    key   = each.value.key
+    value = each.value.value
+
+    propagate_at_launch = false
+  }
+}
---
+++ modules/cloud/aws/eks/variables.tf
@@ -0,0 +1,83 @@
+variable "aws_kms_key" {
+  type = object({
+    description             = string
+    deletion_window_in_days = number
+    enable_key_rotation     = bool
+    tags                    = map(string)
+  })
+}
+
+variable "aws_security_group" {
+  type = object({
+    name_prefix = string
+    vpc_id      = string
+    ingresses = list(object({
+      from_port   = number
+      to_port     = number
+      protocol    = string
+      cidr_blocks = list(string)
+    }))
+    tags = map(string)
+  })
+}
+
+variable "aws_eks_cluster" {
+  type = object({
+    cluster_name                    = string
+    cluster_version                 = string
+    cluster_endpoint_private_access = bool
+    cluster_endpoint_public_access  = bool
+    vpc_id                          = string
+    subnets                         = list(string)
+    cluster_ip_family               = string
+    control_plane_subnet_ids        = list(string)
+    cluster_security_group_additional_rules = map(object({
+      description                = string
+      protocol                   = string
+      from_port                  = number
+      to_port                    = number
+      type                       = string
+      source_node_security_group = bool
+    }))
+    node_security_group_additional_rules = map(object({
+      description      = string
+      protocol         = string
+      from_port        = number
+      to_port          = number
+      type             = string
+      self             = optional(bool)
+      cidr_blocks      = optional(list(string))
+      ipv6_cidr_blocks = optional(list(string))
+    }))
+    eks_managed_node_groups = map(object({
+      create_launch_template = bool
+      launch_template_name   = string
+      ami_type               = string
+      disk_size              = number
+      min_size               = number
+      max_size               = number
+      desired_size           = number
+      capacity_type          = string
+      force_update_version   = bool
+      instance_types         = list(string)
+      labels                 = optional(map(string))
+      taints                 = list(map(string))
+    }))
+    tags = map(any)
+  })
+}
+
+variable "vpc_cni_irsa" {
+  type = object({
+    role_name_prefix      = string
+    attach_vpc_cni_policy = bool
+    vpc_cni_enable_ipv4   = bool
+    tags                  = map(string)
+  })
+  default = {
+    role_name_prefix      = "VPC-CNI-IRSA"
+    attach_vpc_cni_policy = true
+    vpc_cni_enable_ipv4   = true
+    tags                  = {}
+  }
+}
