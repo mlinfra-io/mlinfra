24422120f20ca861b7ea540f08d7136371e1581f
Ali Abbas Jaffri <ali.abbasjaffri@live.com>
2023-12-24 21:17:37 +0000

    Added prerequisite checks before terraform commands
    Signed-off-by: Ali Abbas Jaffri <ali.abbasjaffri@live.com>

--- platinfra_cli/terraform/terraform.py
+++ platinfra_cli/terraform/terraform.py
@@ -1,6 +1,6 @@
 """
-This file is responsible for configuring and running terraform `apply` command
-Apply function undergoes the following steps:
+This file is responsible for configuring and running terraform commands
+Plan function undergoes the following steps:
 - checking if terraform is installed
 - checking if following versions are correct:
     - terraform
@@ -17,31 +17,136 @@ Apply function undergoes the following steps:
         - apply terraform modules with -target
 """
 import os
+import boto3
 import json
+import yaml
 import subprocess
+from botocore.config import Config
 from platinfra_cli.utils.constants import TF_PATH
-from platinfra_cli.utils.utils import terraform_tested_version
-from platinfra_cli.stack_processor.stack_generator import StackGenerator
 from platinfra_cli.terraform.state_helper import StateHelper
+from platinfra_cli.stack_processor.stack_generator import StackGenerator
+from platinfra_cli.utils.utils import terraform_tested_version, clean_tf_directory
 
 
 class Terraform:
     def __init__(self, stack_config_path: str):
         self.stack_config_path = stack_config_path
 
+    def check_terraform_installed(self):
+        try:
+            version = subprocess.check_output(
+                ["terraform", "--version"], universal_newlines=True
+            )
+            installed_version = version.split("\n")[0]
+            if installed_version in terraform_tested_version:
+                return 0
+        except Exception as e:
+            return f"An error occurred while checking the Terraform version: {str(e)}"
+
+    def check_config_file_exists(self):
+        if not os.path.isfile(self.stack_config_path):
+            raise FileNotFoundError(
+                f"The file {self.stack_config_path} does not exist."
+            )
+
+        with open(self.stack_config_path, "r") as stream:
+            try:
+                data = yaml.safe_load(stream)
+            except yaml.YAMLError as exc:
+                print(exc)
+                raise ValueError(f"{exc}")
+
+        required_keys = ["name", "provider", "deployment", "stack"]
+
+        if not all(key in data for key in required_keys):
+            missing_keys = [key for key in required_keys if key not in data]
+            raise ValueError(
+                f"The following keys are missing: {', '.join(missing_keys)}"
+            )
+
+    def clean_mlops_infra_folder(self, delete_dir: bool = True):
+        if delete_dir:
+            clean_tf_directory()
+        else:
+            print(
+                "The param delete_dir is set as false, skipping the directory deletion"
+            )
+
+    def process_config_file(self):
+        file_processor = StackGenerator(stack_config_path=self.stack_config_path)
+        file_processor.generate()
+
+        return file_processor.get_state_file_name(), file_processor.get_region()
+
+    def check_cloud_credentials(self):
+        try:
+            if not boto3.Session().get_credentials():
+                raise ValueError("AWS credentials not found.")
+        except Exception as e:
+            raise ValueError(
+                f"An error occurred while checking AWS credentials: {str(e)}"
+            )
+
+    def check_region_has_three_azs(self, aws_region: str = "eu-central-1"):
+        ec2 = boto3.client("ec2", config=Config(region_name=aws_region))
+        response = ec2.describe_availability_zones(
+            Filters=[{"Name": "zone-type", "Values": ["availability-zone"]}]
+        )
+        azs = [
+            zone["ZoneName"]
+            for zone in response["AvailabilityZones"]
+            if zone["RegionName"] == aws_region
+        ]
+        if len(azs) < 3:
+            raise ValueError(
+                f"""
+                The region {aws_region} has less than three availability zones.
+                You configured {aws_region}, which only has the availability zones: {azs}.
+                Please choose a different region.
+                """
+            )
+
+    def check_terraform_state_storage(self, state_name: str, aws_region: str):
+        state_helper = StateHelper(
+            state=state_name,
+            region=aws_region,
+        )
+
+        state_helper.manage_aws_state_storage()
+
+    def generate_modules_list(self):
+        """
+        This function is responsible for generating a list of modules
+        that will be applied via the -target function of terraform
+        """
+        modules_list = []
+        for file in os.listdir(TF_PATH):
+            if file.endswith(".tf.json"):
+                with open(os.path.join(TF_PATH, file)) as json_file:
+                    data = json.load(json_file)
+                    if "module" in data:
+                        key, _ = data["module"].popitem()
+                        modules_list.append(f"module.{key}")
+        return modules_list
+
+    def generate_terraform_config(self) -> (str, str):
+        self.check_terraform_installed()
+        # TODO: perform this after the cli package has been released
+        # self.check_mlops_cli_installed()
+        self.check_config_file_exists()
+        self.clean_mlops_infra_folder()
+        state_name, aws_region = self.process_config_file()
+        return state_name, aws_region
+
     def plan(self):
         """
         This function is responsible for running terraform plan command
         along with couple of other preleminary checks
         """
-        self.check_terraform_installed()
-        # self.check_mlops_cli_installed()
-        # self.check_config_file_exists()
-        # self.clean_mlops_infra_folder()
-        self.process_config_file()
-        # self.check_cloud_credentials()
-        # self.check_region_has_three_azs()
-        # self.check_terraform_state_storage()
+        state_name, aws_region = self.generate_terraform_config()
+        self.check_cloud_credentials()
+        self.check_region_has_three_azs(aws_region=aws_region)
+        self.check_terraform_state_storage(state_name=state_name, aws_region=aws_region)
         modules_list = self.generate_modules_list()
         return modules_list
 
@@ -55,36 +160,3 @@ class Terraform:
             target_string += f" -target={item}"
 
         return target_string
-
-    def check_terraform_installed(self):
-        try:
-            version = subprocess.check_output(
-                ["terraform", "--version"], universal_newlines=True
-            )
-            installed_version = version.split("\n")[0]
-            if installed_version in terraform_tested_version:
-                return 0
-        except Exception as e:
-            return f"An error occurred while checking the Terraform version: {str(e)}"
-
-    def process_config_file(self):
-        file_processor = StackGenerator(stack_config_path=self.stack_config_path)
-
-        state_helper = StateHelper(
-            state=file_processor.get_state_file_name(),
-            region=file_processor.get_region(),
-        )
-
-        state_helper.manage_aws_state_storage()
-        file_processor.generate()
-
-    def generate_modules_list(self):
-        modules_list = []
-        for file in os.listdir(TF_PATH):
-            if file.endswith(".tf.json"):
-                with open(os.path.join(TF_PATH, file)) as json_file:
-                    data = json.load(json_file)
-                    if "module" in data:
-                        key, _ = data["module"].popitem()
-                        modules_list.append(f"module.{key}")
-        return modules_list
