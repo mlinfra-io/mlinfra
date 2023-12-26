3c8f0620ae2fe92ba794d94135db08ab749f4df3
Ali Abbas Jaffri <ali.abbasjaffri@live.com>
2023-12-26 13:21:19 +0000

    Updated bucket name with bucket prefix and added force_destroy variable
    Signed-off-by: Ali Abbas Jaffri <ali.abbasjaffri@live.com>

--- modules/cloud/aws/s3/s3.tf
+++ modules/cloud/aws/s3/s3.tf
@@ -1,4 +1,5 @@
 resource "aws_s3_bucket" "s3_bucket" {
-  bucket = var.bucket_name
-  tags   = var.tags
+  bucket_prefix = var.bucket_name
+  force_destroy = var.force_destroy
+  tags          = var.tags
 }
