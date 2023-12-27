137989b2ceba402bcc05b89c763e9b56a25511f9
Ali Abbas Jaffri <ali.abbasjaffri@live.com>
2023-12-27 23:16:11 +0000

    Renamed eks output variables
    Signed-off-by: Ali Abbas Jaffri <ali.abbasjaffri@live.com>

--- modules/cloud/aws/eks/tf_module/outputs.tf
+++ modules/cloud/aws/eks/tf_module/outputs.tf
@@ -6,16 +6,20 @@ output "cluster_version" {
   value = module.eks.cluster_version
 }
 
-output "eks_cluster_endpoint" {
+output "cluster_endpoint" {
   value = module.eks.cluster_endpoint
 }
 
-output "eks_cluster_ca_cert" {
+output "cluster_certificate_authority_data" {
   value = module.eks.cluster_certificate_authority_data
 }
 
-output "eks_oidc_provider_arn" {
+output "oidc_provider_arn" {
   value = module.eks.oidc_provider_arn
+}
+
+output "oidc_provider" {
+  value = module.eks.oidc_provider
 }
 
 output "cluster_primary_security_group_id" {
