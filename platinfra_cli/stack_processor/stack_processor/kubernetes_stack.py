8bf4327d0c5f8fc70c99dee64b18855cd52b124a
Ali Abbas Jaffri <ali.abbasjaffri@live.com>
2023-12-27 23:10:29 +0000

    Added checks to outputs
    Signed-off-by: Ali Abbas Jaffri <ali.abbasjaffri@live.com>

--- platinfra_cli/stack_processor/stack_processor/kubernetes_stack.py
+++ platinfra_cli/stack_processor/stack_processor/kubernetes_stack.py
@@ -70,7 +70,7 @@ class KubernetesStack(AbstractStack):
             )
 
             if application_config is not None:
-                if "inputs" in application_config:
+                if "inputs" in application_config and application_config["inputs"]:
                     inputs = {}
                     for input in application_config["inputs"]:
                         if not input["user_facing"]:
@@ -85,7 +85,7 @@ class KubernetesStack(AbstractStack):
                             inputs.update({input_name: input_value})
                         json_module["module"][name].update(inputs)
 
-                if "outputs" in application_config:
+                if "outputs" in application_config and application_config["outputs"]:
                     for output in application_config["outputs"]:
                         if output["export"]:
                             output_val = "${ %s }" % f"module.{name}.{output['name']}"
