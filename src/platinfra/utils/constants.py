f2ea92f11e51a77089f674d45d8e7e7c74e1dd79
Ali Abbas Jaffri <ali.abbasjaffri@live.com>
2024-01-07 16:03:06 +0000

    Updated code
    Signed-off-by: Ali Abbas Jaffri <ali.abbasjaffri@live.com>

--- src/platinfra/utils/constants.py
+++ src/platinfra/utils/constants.py
@@ -1,3 +1,4 @@
+# TODO: update this to path.cwd
 TF_PATH = "./.mlops_stack"
 
 CREATE_VPC_DB_SUBNET = ["remote_tracking"]
--- src/platinfra/utils/utils.py
+++ src/platinfra/utils/utils.py
@@ -40,3 +40,31 @@ def check_terraform_version():
 def terraform_tested_version():
     # TODO: read from a file and then populate this field
     return "1.6.3"
+
+
+# def _verify_aws_cloud_credentials(self) -> None:
+#     ensure_installed("aws")
+#     try:
+#         aws_caller_identity = boto3.client("sts").get_caller_identity()
+#         configured_aws_account_id = aws_caller_identity["Account"]
+#         required_aws_account_id = self.root().providers["aws"]["account_id"]
+#         if required_aws_account_id != configured_aws_account_id:
+#             raise UserErrors(
+#                 "\nSystem configured AWS Credentials are different from the ones being used in the "
+#                 f"Configuration. \nSystem is configured with credentials for account "
+#                 f"{configured_aws_account_id} but the config requires the credentials for "
+#                 f"{required_aws_account_id}."
+#             )
+#     except NoCredentialsError:
+#         raise UserErrors(
+#             "Unable to locate credentials.\n"
+#             "Visit `https://docs.aws.amazon.com/sdk-for-java/v1/developer-guide/setup-credentials.html` "
+#             "for more information."
+#         )
+#     except ClientError as e:
+#         raise UserErrors(
+#             "The AWS Credentials are not configured properly.\n"
+#             f" - Code: {e.response['Error']['Code']} Error Message: {e.response['Error']['Message']}"
+#             "Visit `https://docs.aws.amazon.com/sdk-for-java/v1/developer-guide/setup-credentials.html` "
+#             "for more information."
+#         )
