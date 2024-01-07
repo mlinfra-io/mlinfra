c2aeec038fca24c30f73a45d863e9db3bf16502d
Ali Abbas Jaffri <ali.abbasjaffri@live.com>
2024-01-07 16:02:47 +0000

    Added absolute and relative paths
    Signed-off-by: Ali Abbas Jaffri <ali.abbasjaffri@live.com>

--- src/platinfra/__init__.py
+++ src/platinfra/__init__.py
@@ -1,13 +1,15 @@
 from pathlib import Path
 
 
-# PROJECT_ROOT = Path(__file__).absolute().parent
 def absolute_project_root() -> Path:
+    """
+    Returns the absolute path to the project root.
+    """
     return Path(__file__).absolute().parent
 
 
 def relative_project_root() -> Path:
+    """
+    Returns the relative path to the project root.
+    """
     return Path(__file__).relative_to(Path(__file__).parent.parent).parent
-
-
-PROJECT_ROOT = Path(__file__).relative_to(Path(__file__).parent.parent).parent
