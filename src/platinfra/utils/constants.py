b543c0bf71ad2730a4db929ae1b4a0ab89ecb186
Ali Abbas Jaffri <ali.abbasjaffri@live.com>
2024-01-25 22:48:34 +0100

    Updated constants
    Signed-off-by: Ali Abbas Jaffri <ali.abbasjaffri@live.com>

--- src/platinfra/utils/constants.py
+++ src/platinfra/utils/constants.py
@@ -10,8 +10,8 @@
 #     or implied. See the License for the specific language governing
 #     permissions and limitations under the License.
 
+from time import time
 from typing import Final
-from uuid import uuid4
 
 import toml
 
@@ -22,8 +22,8 @@ CREATE_VPC_DB_SUBNET = ["remote_tracking"]
 
 DEV_VERSION: Final = "dev"
 PLATINFRA_DISABLE_REPORTING: Final = "PLATINFRA_DISABLE_REPORTING"
-SESSION_ID: Final = uuid4()
+SESSION_ID: Final = int(time() * 1000)
 
-with open("pyproject.toml", "r") as pyproject_toml_file:
+with open("../pyproject.toml", "r") as pyproject_toml_file:
     pyproj_toml_data = toml.load(pyproject_toml_file)
-VERSION: Final = pyproj_toml_data["version"]
+VERSION: Final = pyproj_toml_data["project"]["version"]
