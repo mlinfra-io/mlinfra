912bd2efe22d14e91217d98594dad8e0ff563b0e
Ali Abbas Jaffri <ali.abbasjaffri@live.com>
2024-06-13 23:20:28 +0200

    Updated variables
    Signed-off-by: Ali Abbas Jaffri <ali.abbasjaffri@live.com>

--- src/mlinfra/modules/local/kind/k8s/tf_module/variables.tf
+++ src/mlinfra/modules/local/kind/k8s/tf_module/variables.tf
@@ -1,4 +1,4 @@
-variable "name" {
+variable "cluster_name" {
   type        = string
   description = "Name of the local KinD cluster"
   default     = "kind-cluster"
@@ -18,4 +18,15 @@ variable "node_image" {
 variable "k8s_version" {
   type        = string
   description = "Defines kubernetes version for the KinD cluster"
+}
+
+variable "wait_for_control_plane" {
+  type        = bool
+  description = "Whether to wait for control plane to be available before creating dataplane"
+}
+
+variable "kubeconfig_path" {
+  type        = string
+  description = "Path of kubeconfig after getting created"
+  default     = null
 }
