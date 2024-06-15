1eff46af6e24a3e3cfb1a61eea204e116d841dc5
Ali Abbas Jaffri <ali.abbasjaffri@live.com>
2024-06-15 16:51:58 +0200

    Added defaults to kind module
    Signed-off-by: Ali Abbas Jaffri <ali.abbasjaffri@live.com>

--- src/mlinfra/modules/local/kind/k8s/tf_module/variables.tf
+++ src/mlinfra/modules/local/kind/k8s/tf_module/variables.tf
@@ -18,11 +18,13 @@ variable "node_image" {
 variable "k8s_version" {
   type        = string
   description = "Defines kubernetes version for the KinD cluster"
+  default     = "1.29"
 }
 
 variable "wait_for_control_plane" {
   type        = bool
   description = "Whether to wait for control plane to be available before creating dataplane"
+  default     = false
 }
 
 variable "kubeconfig_path" {
