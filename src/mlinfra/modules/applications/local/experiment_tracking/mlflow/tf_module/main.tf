0a7005be20c8c576b780bc9541c1c2713e37be92
Ali Abbas Jaffri <ali.abbasjaffri@live.com>
2025-01-27 23:56:17 +0100

    Added minor fix to password gen
    Signed-off-by: Ali Abbas Jaffri <ali.abbasjaffri@live.com>

--- src/mlinfra/modules/applications/local/experiment_tracking/mlflow/tf_module/main.tf
+++ src/mlinfra/modules/applications/local/experiment_tracking/mlflow/tf_module/main.tf
@@ -1,7 +1,7 @@
 resource "random_password" "mlflow_password" {
   length           = 16
   special          = true
-  override_special = "!#$%&*()-_=+[]{}<>"
+  override_special = "_+=()"
 }
 
 locals {
