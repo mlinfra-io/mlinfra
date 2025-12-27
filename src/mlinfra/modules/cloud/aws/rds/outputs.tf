1a7fa19f3de4e65c59382a5a168c3075650b8af6
Ali Abbas Jaffri <ali.abbasjaffri@live.com>
2025-12-27 16:48:04 +0100

    Updated module to fetch rds password
    Signed-off-by: Ali Abbas Jaffri <ali.abbasjaffri@live.com>

--- src/mlinfra/modules/cloud/aws/rds/outputs.tf
+++ src/mlinfra/modules/cloud/aws/rds/outputs.tf
@@ -3,7 +3,12 @@ output "db_instance_username" {
 }
 
 output "db_instance_password" {
-  value = module.rds.db_instance_password
+  value     = var.create_rds ? jsondecode(data.aws_secretsmanager_secret_version.db_password[0].secret_string)["password"] : null
+  sensitive = true
+}
+
+output "db_instance_password_arn" {
+  value = module.rds.db_instance_master_user_secret_arn
 }
 
 output "db_instance_endpoint" {
